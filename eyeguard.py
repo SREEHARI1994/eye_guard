04be96bdfd922e8157cc03a00cb1eb1cbe13093f
SREEHARI1994 <sreehari.mras@gmail.com>
2024-02-19 00:23:51 +0530

    changinging paths to audio files

--- eyeguard.py
+++ eyeguard.py
@@ -21,17 +21,17 @@ elif len(sys.argv)==2:
 		minutes=2
 
 def sound_alarm():
-	playsound('E:/files/stdy/Useful Scripts/eye_guard/alarm.mp3')#path to your file
+	playsound('alarm.mp3')#path to your file
 
 def sound_resume_work_alert():
-	playsound('E:/files/stdy/Useful Scripts/eye_guard/resume.mp3')#path to your file
+	playsound('resume.mp3')#path to your file
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
 if __name__=="__main__":
-	playsound('E:/files/stdy/Useful Scripts/eye_guard/greeting.mp3') #path to your file
+	playsound('greeting.mp3') #path to your file
 	schedule.every(minutes).minutes.do(run_threaded,sound_alarm)
 	time.sleep(seconds)
 	schedule.every(minutes).minutes.do(run_threaded,sound_resume_work_alert)
